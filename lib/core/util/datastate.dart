7101cbe804ee5b88c9e20fbd6c99d4dbb62e32eb
Arkariz <muhammadrisky1401@gmail.com>
2022-08-04 08:49:14 +0700

    add datastate utils

---
+++ lib/core/util/datastate.dart
@@ -0,0 +1,14 @@
+abstract class DataState<T> {
+  T? data;
+  String? error;
+
+  DataState({this.data, this.error});
+}
+
+class DataSuccess<T> extends DataState<T> {
+  DataSuccess(T data) : super(data: data);
+}
+
+class DataFailed<T> extends DataState<T> {
+  DataFailed(String error) : super(error: error);
+}
